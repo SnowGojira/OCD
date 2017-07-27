7a4934380c5dfb4dcea94791709bc2089a565389
Haku Hal <hakuhal.snow@gmail.com>
2017-07-28 01:34:33 +0800

    fit the result page style

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
  * Created by hakuh on 2017/7/10.
  */
 
-var str_light='<p class="result">你的强迫症指数<span>%data%</span>分<br>是特新型手机病毒强迫症的早期患者<br>好棒棒，像你这样的人才已经不多了~</p>';
-var str_medium='<p class="result">你的强迫症指数<span>%data%</span>分<br>是特新型手机病毒强迫症的中期患者<br>少低头，否则皇冠会掉、吃枣药丸</p>';
-var str_heavy='<p class="result">你的强迫症指数<span>%data%</span>分<br>是特新型手机病毒强迫症的晚期患者<br>治愈的可能性很小，请尽量保持心情愉快</p>';
-var str_survive='<p class="result">你的强迫症指数<span>%data%</span>分<br>特新型手机病毒强迫症的免疫者<br>天啊，你还是人吗？</p>';
+var str_light='<p class="diagnosis">你的强迫症指数为<span>%data%</span>分<br>是特新型手机病毒强迫症的早期患者<br>好棒棒，像你这样的人才已经不多了~</p>';
+var str_medium='<p class="diagnosis">你的强迫症指数为<span>%data%</span>分<br>是特新型手机病毒强迫症的中期患者<br>少低头，否则皇冠会掉、吃枣药丸</p>';
+var str_heavy='<p class="diagnosis">你的强迫症指数为<span>%data%</span>分<br>是特新型手机病毒强迫症的晚期患者<br>治愈的可能性很小，请尽量保持心情愉快</p>';
+var str_survive='<p class="diagnosis">你的强迫症指数为<span>%data%</span>分<br>特新型手机病毒强迫症的免疫者<br>天啊，你还是人吗？</p>';
 
 
 $(function () {
